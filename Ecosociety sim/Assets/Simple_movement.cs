c89e14a69146f4372cba3fecf40cef4cdc3718f0
Sofia Bzhilyanskaya <sab4633@rit.edu>
2020-08-10 19:34:17 -0400

    Rewrote movement class to be more simple

---
+++ Ecosociety sim/Assets/Simple_movement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simple_movement : MonoBehaviour
+{
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentPos = GetComponent<Rigidbody>().position;
+        GetComponent<Rigidbody>().position = new Vector3(currentPos.x + 0.05f, currentPos.y,currentPos.z+0.05f);
+    }
+}
