5864ade0af1821e1376c408c10355038e03e01b5
Sofia Bzhilyanskaya <sab4633@rit.edu>
2020-08-10 19:48:39 -0400

    Directed Positive Movement

--- Ecosociety sim/Assets/Simple_movement.cs
+++ Ecosociety sim/Assets/Simple_movement.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class Simple_movement : MonoBehaviour
 {
-  
 
+    float toX;
+    float toZ;
     // Update is called once per frame
     void Update()
     {
         Vector3 currentPos = GetComponent<Rigidbody>().position;
-        GetComponent<Rigidbody>().position = new Vector3(currentPos.x + 0.05f, currentPos.y,currentPos.z+0.05f);
+        toX = 0f;
+        toZ = 0f;
+        if (Random.Range(0, 2) == 0)
+      //  if(true)
+        {
+
+            toZ = 0.05f;
+        }
+        else
+        {
+            toX = 0.05f;
+        }
+
+        GetComponent<Rigidbody>().position = new Vector3(currentPos.x + toX, currentPos.y,currentPos.z + toZ);
+        
     }
+
+    
 }
